80579925930a44435d92b18d38e99657ab4b300c
pregnito <pregnito@gmail.com>
2017-11-11 19:07:51 +0100

    new repository setup

---
+++ index.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+
+
+  </head>
+
+  <body>
+
+  <nav class="navbar navbar-toggleable-md navbar-inverse bg-inverse">
+  
+  <a class="navbar-brand" href="index.php">Hotel Pregnito</a>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="reservation.php">Reservation </a>
+      </li>
+
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="text" placeholder="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+
+
+<div class="jumbotron" id="top">
+	<h4 class="text-center">Hello, Welcome to</h4>
+	
+	<hr>
+
+	<h1 class="text-center">Hotel Pregnito</h1>
+
+	<br>
+
+		<h5 class="text-center">HOTEL | SPA | GYM | FINE DINING</h5>
+		
+</div>
+
+<br>
+<br>
+		
+
+<div class="row">
+	<h1 class="text-center">Book a package</h1> 
+
+	<br>
+	<br>
+
+	<p>Sed euismod sem id consequat rutrum. Ut convallis lorem a orci mollis, eu vulputate libero aliquet. Praesent egestas nisi sed purus tincidunt faucibus. Aliquam lobortis orci lacus, sed faucibus augue dapibus vitae. Ut vitae mi sapien. Phasellus a eros justo. Curabitur odio massa, tincidunt nec nibh sit amet</p>
+	
+</div>
+
+<br>
+<br>
+
+<div class="row text-center">
+
+	<h1 class="text-center"> Our Room Types </h1>
+
+	<div class="row">
+		<div class="col-md-4">
+
+<br>
+
+			<img src="images/pic4.jpg" />
+			<h4>Standard Double Room</h4>
+			<p>Sed euismod sem id consequat rutrum. Ut convallis lorem a orci mollis, eu vulputate libero aliquet. Praesent egestas nisi sed purus tincidunt faucibus. Aliquam lobortis orci lacus, sed faucibus augue dapibus vitae. Ut vitae mi sapien. Phasellus a eros justo. Curabitur odio massa, tincidunt nec nibh sit amet</p>
+		</div>
+
+		<div class="col-md-4">
+			<img src="images/pic3.jpg" />
+			<h4>Superior Double Room</h4>
+			<p>Sed euismod sem id consequat rutrum. Ut convallis lorem a orci mollis, eu vulputate libero aliquet. Praesent egestas nisi sed purus tincidunt faucibus. Aliquam lobortis orci lacus, sed faucibus augue dapibus vitae. Ut vitae mi sapien. Phasellus a eros justo. Curabitur odio massa, tincidunt nec nibh sit amet</p>
+		</div>
+
+
+			<div class="col-md-4">
+		<img src="images/pic6.jpg" />
+		<h4>Standard Single Room</h4>
+		<p>Sed euismod sem id consequat rutrum. Ut convallis lorem a orci mollis, eu vulputate libero aliquet. Praesent egestas nisi sed purus tincidunt faucibus. Aliquam lobortis orci lacus, sed faucibus augue dapibus vitae. Ut vitae mi sapien. Phasellus a eros justo. Curabitur odio massa, tincidunt nec nibh sit amet</p>
+		</div>
+
+
+
+		<div class="col-md-4">
+			<img src="images/pic2.jpg" />
+			<h4>Superior Single Room</h4>
+			<p>Sed euismod sem id consequat rutrum. Ut convallis lorem a orci mollis, eu vulputate libero aliquet. Praesent egestas nisi sed purus tincidunt faucibus. Aliquam lobortis orci lacus, sed faucibus augue dapibus vitae. Ut vitae mi sapien. Phasellus a eros justo. Curabitur odio massa, tincidunt nec nibh sit amet</p>
+		</div>
+
+		<div class="col-md-4">
+			<img src="images/pic1.jpg" />
+			<h4>VIP Room</h4>
+			<p>Sed euismod sem id consequat rutrum. Ut convallis lorem a orci mollis, eu vulputate libero aliquet. Praesent egestas nisi sed purus tincidunt faucibus. Aliquam lobortis orci lacus, sed faucibus augue dapibus vitae. Ut vitae mi sapien. Phasellus a eros justo. Curabitur odio massa, tincidunt nec nibh sit amet</p>	
+		</div>
+
+
+
+		<div class="col-md-4">
+			<img src="images/pic1.jpg" />
+			<h4>VIP Room</h4>
+			<p>Sed euismod sem id consequat rutrum. Ut convallis lorem a orci mollis, eu vulputate libero aliquet. Praesent egestas nisi sed purus tincidunt faucibus. Aliquam lobortis orci lacus, sed faucibus augue dapibus vitae. Ut vitae mi sapien. Phasellus a eros justo. Curabitur odio massa, tincidunt nec nibh sit amet</p>	
+		</div>
+		
+	
+	</div>
+</div>
+
+<div class="footer">
+	<div class="text-center">
+		With love from PREGNITO
+	</div>
+</div>
+
+<link href="bootstrap/js/bootstrap.min.js" rel="stylesheet">
+
+ </body>
+</html>
---
+++ reservation.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+
+
+  </head>
+
+  <body>
+
+  <nav class="navbar navbar-toggleable-md navbar-inverse bg-inverse">
+  
+  <a class="navbar-brand" href="index.php">Hotel Pregnito</a>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="reservation.php">Reservation </a>
+      </li>
+
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="text" placeholder="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+
+<div class="row">
+  <div class="col-md-3">Reserve Your Room </div>
+  <div class="col-md-6">Reserve Your Room </div>
+<div class="col-md-8">Reserve Your Room </div>
+
+  
+  
+</div>
+
+
+<link href="bootstrap/js/bootstrap.min.js" rel="stylesheet">
+
+ </body>
+</html>
